dc18b3b8887c72d395c5da9b66e6a1d01771a5f2
Sereinity <sereinity@sereinity.fr>
2018-11-10 16:50:27 +0100

    Use a vector crate

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+extern crate rulinalg;
+
+use rulinalg::vector::Vector;
+
 fn main() {
     let nx = 200;
     let ny = 100;
     println!("P3\n{} {}\n255", nx, ny);
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let ir = ((i as f64 / nx as f64)*255.99) as usize;
-            let ig = ((j as f64 / ny as f64)*255.99) as usize;
-            let ib = (0.2*255.99) as usize;
-            println!("{} {} {}", ir, ig, ib);
+            let mut v: Vec<f64> = Vec::with_capacity(3);
+            v.push(i as f64 / nx as f64);
+            v.push(j as f64 / ny as f64);
+            v.push(0.2);
+            let v = Vector::new(v);
+            let iv = v * 255.99;
+            println!("{} {} {}", iv[0] as usize, iv[1] as usize, iv[2] as usize);
         }
     }
 }
