50aaf0bc36cf48c739f05930edb4d59c801d0369
Sereinity <sereinity@sereinity.fr>
2018-11-10 16:40:19 +0100

    Output an image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let nx = 200;
+    let ny = 100;
+    println!("P3\n{} {}\n255", nx, ny);
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let ir = ((i as f64 / nx as f64)*255.99) as usize;
+            let ig = ((j as f64 / ny as f64)*255.99) as usize;
+            let ib = (0.2*255.99) as usize;
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
